ac01b36e2394422efe065be7c42a0f05013f4cf3
Fernando <ffmp777@yahoo.com>
2017-05-13 17:49:31 -0600

    created the routes that will be used in the app 'polls', 'poll', and 'results'

---
+++ app/routes/polls/poll.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model() {} // "Short circuits" amber data, which was pulled in when the app was created, but will not be used in this project.
+});
